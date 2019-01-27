62bd63df1766f186fe3f6cd0fe9765d0fd07d4ce
Maximilian Held <info@maxheld.de>
2019-01-27 17:12:14 +0100

    change order of ssh-add

--- rsync/entrypoint.sh
+++ rsync/entrypoint.sh
@@ -15,11 +15,11 @@ chmod 600 "$SSH_PATH/known_hosts"
 chmod 600 "$SSH_PATH/deploy_key"
 chmod 600 "$SSH_PATH/deploy_key.pub"
 
-eval $(ssh-agent)
-ssh-add "$SSH_PATH/deploy_key"
-
 # below key was created by running the below line from inside karli.rrze
 # ssh-keyscan -t ecdsa-sha2-nistp256 karli.rrze.uni-erlangen.de
 echo '$HOST,131.188.16.138 ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBFHJVSekYKuF5pMKyHe1jS9mUkXMWoqNQe0TTs2sY1OQj379e6eqVSqGZe+9dKWzL5MRFpIiySRKgvxuHhaPQU4=' >> "$SSH_PATH/known_hosts"
+
+eval $(ssh-agent)
+ssh-add "$SSH_PATH/deploy_key"
 
 rsync -r --delete-after --quiet home/repo/_site/ $USER@$HOST:/proj/websource/docs/FAU/fakultaet/phil/www.datascience.phil.fau.de/websource/ghaction-rsync
